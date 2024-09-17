74f45ddf17a3b0c4910cd8a82c5dc50ceee516c3
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-09-17 11:33:58 +0200

    [FIX]: deploy

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,22 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function registerBundles(): iterable
+    {
+        $contents = require $this->getProjectDir().'/config/bundles.php';
+        foreach ($contents as $class => $envs) {
+            if ($envs[$this->environment] ?? $envs['all']) {
+                yield new $class();
+            }
+        }
+    }
 }
