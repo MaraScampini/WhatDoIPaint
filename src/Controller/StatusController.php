566378fe27093d9636633c8fe3482d73c4ccc6a7
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-09-18 11:07:19 +0200

    [TEST]

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -7,18 +7,14 @@ namespace App\Controller;
 use App\Utilities\StaticUtilities;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api')]
 class StatusController extends AbstractController
 {
     #[Route('/status/selector')]
-    public function getStatusSelector(Request $request): JsonResponse
+    public function getStatusSelector(): JsonResponse
     {
-        $headers = $request->headers->get('Authorization');
-
-        return new JsonResponse('The header is ' . $headers);
-
         $status = StaticUtilities::getStatusSelector();
         return new JsonResponse($status);
     }
