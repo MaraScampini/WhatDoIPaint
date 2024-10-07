81b1c7f16fb092e2edceef678c80e2738fe71c7c
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-10-07 09:36:59 +0200

    [FIX]: change how elements are added

--- src/Service/Project/ProjectService.php
+++ src/Service/Project/ProjectService.php
@@ -35,22 +35,25 @@ class ProjectService implements ProjectServiceInterface
 {
     private User $user;
     private Project $newProject;
+
     public function __construct(
-        private readonly UserProjectsRepositoryInterface $userProjectsRE,
-        private readonly BrandRepositoryInterface $brandRE,
-        private readonly StatusRepositoryInterface $statusRE,
-        private readonly ProjectRepositoryInterface $projectRE,
-        private readonly UserRepositoryInterface $userRE,
-        private readonly LevelRepositoryInterface $levelRE,
-        private readonly TechniqueRepositoryInterface $techniqueRE,
+        private readonly UserProjectsRepositoryInterface     $userProjectsRE,
+        private readonly BrandRepositoryInterface            $brandRE,
+        private readonly StatusRepositoryInterface           $statusRE,
+        private readonly ProjectRepositoryInterface          $projectRE,
+        private readonly UserRepositoryInterface             $userRE,
+        private readonly LevelRepositoryInterface            $levelRE,
+        private readonly TechniqueRepositoryInterface        $techniqueRE,
         private readonly ProjectTechniqueRepositoryInterface $projectTechniqueRepository,
-        private readonly ElementRepositoryInterface $elementRepository,
-        private readonly SquadRepositoryInterface $squadRepository,
-        private readonly ImageRepositoryInterface $imageRepository,
-        private readonly EntityManagerInterface $em,
-        private readonly ImgurService $imgurSE,
-        private readonly UpdateServiceInterface $updateService
-    ) {}
+        private readonly ElementRepositoryInterface          $elementRepository,
+        private readonly SquadRepositoryInterface            $squadRepository,
+        private readonly ImageRepositoryInterface            $imageRepository,
+        private readonly EntityManagerInterface              $em,
+        private readonly ImgurService                        $imgurSE,
+        private readonly UpdateServiceInterface              $updateService
+    )
+    {
+    }
 
     public function createProject(array $projectData, User $user): void
     {
@@ -68,7 +71,7 @@ class ProjectService implements ProjectServiceInterface
             ->setCreationDate(new \DateTime())
             ->setLastUpdate(new \DateTime());
 
-        if(isset($projectData['image']) && $projectData['image'] !== "") {
+        if (isset($projectData['image']) && $projectData['image'] !== "") {
             $imageURL = $this->imgurSE->uploadImage($projectData['image']);
             $image = new Image();
             $image->setUrl($imageURL);
@@ -77,7 +80,7 @@ class ProjectService implements ProjectServiceInterface
             $this->em->persist($image);
         }
 
-        if(isset($projectData['description']) && $projectData['description'] !== "") $project->setDescription($projectData['description']);
+        if (isset($projectData['description']) && $projectData['description'] !== "") $project->setDescription($projectData['description']);
 
         $this->em->persist($project);
         $this->newProject = $project;
@@ -85,15 +88,15 @@ class ProjectService implements ProjectServiceInterface
         $userProject = $this->addUser();
         $userProject->setPriority($projectData['priority']);
 
-        if(isset($projectData['users']) && count($projectData['users']) > 0) {
+        if (isset($projectData['users']) && count($projectData['users']) > 0) {
             $users = $projectData['users'];
-            foreach($users as $userId) {
+            foreach ($users as $userId) {
                 $this->addUser($userId);
             }
         }
 
-        if(isset($projectData['techniques']) && count($projectData['techniques']) > 0) {
-            foreach($projectData['techniques'] as $techniqueId) {
+        if (isset($projectData['techniques']) && count($projectData['techniques']) > 0) {
+            foreach ($projectData['techniques'] as $techniqueId) {
                 $technique = $this->techniqueRE->find($techniqueId);
                 $projectTechnique = new ProjectTechnique();
                 $projectTechnique->setProject($project);
@@ -109,7 +112,7 @@ class ProjectService implements ProjectServiceInterface
         $userProject->setProject($this->newProject)
             ->setIncorporationDate(new \DateTime());
 
-        if(!$userId) {
+        if (!$userId) {
             $userProject->setUser($this->user);
         } else {
             $user = $this->userRE->find($userId);
@@ -133,7 +136,7 @@ class ProjectService implements ProjectServiceInterface
         $project = $this->projectRE->find($projectId);
 
         $isInProject = $this->checkIfUserAlreadyInProject($user, $project);
-        if($isInProject) throw new CustomMessageException('User is already in that project');
+        if ($isInProject) throw new CustomMessageException('User is already in that project');
 
         $userProject = new UserProjects();
         $userProject->setProject($project)
@@ -146,7 +149,7 @@ class ProjectService implements ProjectServiceInterface
     private function checkIfUserAlreadyInProject(User $user, Project $project): bool
     {
         $userProject = $this->userProjectsRE->findOneBy(['user' => $user, 'project' => $project]);
-        if($userProject instanceof UserProjects) return true;
+        if ($userProject instanceof UserProjects) return true;
         return false;
     }
 
@@ -154,43 +157,43 @@ class ProjectService implements ProjectServiceInterface
     {
         $projectId = $newProjectData['projectId'];
         $project = $this->projectRE->find($projectId);
-        if(!$project instanceof Project) throw new EntityNotFoundException('Project');
+        if (!$project instanceof Project) throw new EntityNotFoundException('Project');
 
-        if(isset($newProjectData['status'])) {
+        if (isset($newProjectData['status'])) {
             $status = $this->statusRE->find($newProjectData['status']);
-            if(!$status instanceof Status) throw new EntityNotFoundException('Status');
+            if (!$status instanceof Status) throw new EntityNotFoundException('Status');
             $project->setStatus($status);
         }
 
-        if(isset($newProjectData['level'])) {
+        if (isset($newProjectData['level'])) {
             $level = $this->levelRE->find($newProjectData['level']);
-            if(!$level instanceof Level) throw new EntityNotFoundException('Level');
+            if (!$level instanceof Level) throw new EntityNotFoundException('Level');
             $project->setLevel($level);
         }
 
-        if(isset($newProjectData['brand'])) {
+        if (isset($newProjectData['brand'])) {
             $brand = $this->brandRE->find($newProjectData['brand']);
-            if(!$brand instanceof Brand) throw new EntityNotFoundException('Brand');
+            if (!$brand instanceof Brand) throw new EntityNotFoundException('Brand');
             $project->setBrand($brand);
         }
 
-        if(isset($newProjectData['name'])) {
+        if (isset($newProjectData['name'])) {
             $project->setName($newProjectData['name']);
         }
 
-        if(isset($newProjectData['description'])) {
+        if (isset($newProjectData['description'])) {
             $project->setDescription($newProjectData['description']);
         }
 
-        if(isset($newProjectData['isPriority'])) {
+        if (isset($newProjectData['isPriority'])) {
             $project->setPriority($newProjectData['isPriority']);
         }
 
-        if(isset($newProjectData['image'])) {
+        if (isset($newProjectData['image'])) {
             $imageURL = $this->imgurSE->uploadImage($newProjectData['image']);
-            if($imageURL) {
+            if ($imageURL) {
                 $previousImage = $this->imageRepository->findOneBy(['project' => $project]);
-                if($previousImage instanceof Image) $this->em->remove($previousImage);
+                if ($previousImage instanceof Image) $this->em->remove($previousImage);
             }
             $image = new Image();
             $image->setProject($project);
@@ -207,7 +210,7 @@ class ProjectService implements ProjectServiceInterface
     public function togglePriority(string $projectId, User $user): void
     {
         $userProject = $this->userProjectsRE->find($projectId);
-        if(!$userProject instanceof UserProjects || $userProject->getUser() !== $user) throw new CustomMessageException('You cannot edit that project');
+        if (!$userProject instanceof UserProjects || $userProject->getUser() !== $user) throw new CustomMessageException('You cannot edit that project');
 
         $projectPriority = $userProject->isPriority();
         $userProject->setPriority(!$projectPriority);
@@ -238,7 +241,7 @@ class ProjectService implements ProjectServiceInterface
 
     private function getElementsBySquad(array $squads): array
     {
-        foreach($squads as &$squad) {
+        foreach ($squads as &$squad) {
             $squadId = $squad['id'];
             $elements = $this->elementRepository->getElementsBySquad($squadId);
             $squad['elements'] = $elements;
@@ -254,33 +257,36 @@ class ProjectService implements ProjectServiceInterface
     {
         $projectId = $elementsData['projectId'];
         $project = $this->projectRE->find($projectId);
-        if(!$project instanceof Project) throw new EntityNotFoundException('project');
-        $elementsToAdd = $elementsData['elements'];
+        if (!$project instanceof Project) throw new EntityNotFoundException('project');
+        $statuses = $elementsData['statuses'];
 
-        foreach($elementsToAdd as $newElement) {
-            $status = $this->statusRE->find($newElement['status']);
-            if($newElement['amount'] > 1) {
-                $squad = new Squad();
-                $squad->setProject($project);
-                $squad->setLastUpdate(new \DateTime());
-                $squad->setName($newElement['name']);
-                $this->em->persist($squad);
-                for ($i = 1; $i <= $newElement['amount']; $i++) {
+        foreach ($statuses as $status) {
+            $statusEntity = $this->statusRE->find($status['id']);
+            $elementsToAdd = $status['elements'];
+            foreach ($elementsToAdd as $newElement) {
+                if ($newElement['amount'] > 1) {
+                    $squad = new Squad();
+                    $squad->setProject($project);
+                    $squad->setLastUpdate(new \DateTime());
+                    $squad->setName($newElement['name']);
+                    $this->em->persist($squad);
+                    for ($i = 1; $i <= $newElement['amount']; $i++) {
+                        $element = new Element();
+                        $element->setProject($project);
+                        $element->setStatus($statusEntity);
+                        $element->setName($newElement['name'] . ' ' . $i);
+                        $element->setLastUpdate(new \DateTime());
+                        $element->setSquad($squad);
+                        $this->em->persist($element);
+                    }
+                } else {
                     $element = new Element();
                     $element->setProject($project);
-                    $element->setStatus($status);
-                    $element->setName($newElement['name'] . ' ' . $i);
+                    $element->setStatus($statusEntity);
+                    $element->setName($newElement['name']);
                     $element->setLastUpdate(new \DateTime());
-                    $element->setSquad($squad);
                     $this->em->persist($element);
                 }
-            } else {
-                $element = new Element();
-                $element->setProject($project);
-                $element->setStatus($status);
-                $element->setName($newElement['name']);
-                $element->setLastUpdate(new \DateTime());
-                $this->em->persist($element);
             }
         }
     }
